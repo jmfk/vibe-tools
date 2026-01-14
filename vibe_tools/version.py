bf4ee92b9014c1ce4e7f4eeb7251c51ef88c332f
Johan Karlsson <johan@colliberty.com>
2026-01-14 21:26:21 +0100

    vibe deps to update makefile

--- vibe_tools/version.py
+++ vibe_tools/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.37.dev1+gd9ac2ce96.d20260114'
-__version_tuple__ = version_tuple = (0, 0, 37, 'dev1', 'gd9ac2ce96.d20260114')
+__version__ = version = '0.0.51.dev0+g5335e7491.d20260114'
+__version_tuple__ = version_tuple = (0, 0, 51, 'dev0', 'g5335e7491.d20260114')
 
-__commit_id__ = commit_id = 'gd9ac2ce96'
+__commit_id__ = commit_id = 'g5335e7491'
